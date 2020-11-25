e7c3d3c52e6ec0e1c0814aeb6248ded303683454
AlondraCordoba <alondracordoba0720@gmail.com>
2020-11-24 19:50:47 -0600

    Segundo commit (parte de maquetado, creacion de; pages, services y routes.ts)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './Pages/users/users.component';
+import { FormularioComponent } from './Pages/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+// Importaciones de componentes
+
+export const routes: Routes = [
+   // { path: '', component: }
+   // Rutas
+];
